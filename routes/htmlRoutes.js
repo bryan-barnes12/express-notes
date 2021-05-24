17f8e8099f80440e8a2e1c3b607d1146f0767b81
Bryan Barnes <bryan.barnes12@gmail.com>
2021-05-24 16:42:39 -0500

    Updated basic functionality (mvp)

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,25 +1,16 @@
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
-
+const express = require('express');
 const path = require('path');
 
-// ROUTING
-
 module.exports = (app) => {
-  // => HTML GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
+  const router = express.Router();
 
-  app.get('/tables', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/tables.html'));
+  router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
   });
 
-  app.get('/reserve', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/reserve.html'));
+  router.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
   });
 
-  // If no matching route is found default to home
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/home.html'));
-  });
+  app.use(router);
 };
